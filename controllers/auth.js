a1aa34739ce2b0166c2679c2350562565e58e01b
Samuel Osinubi <samobafemisayo@gmail.com>
2023-09-02 20:11:16 +0100

    update7

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const crypto = require("crypto");
+const jwt = require('jsonwebtoken');
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const User = require("../models/user");
@@ -15,6 +16,7 @@ exports.getMe = async (req, res, next) => {
       return next(new ErrorResponse(`User not found`, 404));
     }
 
+
     res.status(200).json({
       status: 'success',
       data: {
@@ -28,4 +30,25 @@ exports.getMe = async (req, res, next) => {
       message: err.message
     });
   }
+};
+
+const setTokenCookieAndRespond = (user, statusCode, res) => {
+  const token = jwt.sign({ id: user.id }, `${process.env.JWT_SECRET}`);
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000, // Set the cookie expiration time (e.g., 1 day)
+    secure: process.env.NODE_ENV === 'production' // Set 'secure' to true in production
+  });
+
+  res.status(statusCode).json({
+    success: true,
+    token: token,
+    data: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role
+    }
+  });
 };
