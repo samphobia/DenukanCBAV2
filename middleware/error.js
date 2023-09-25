e927a6a6845e116990d6929cb1f2ef0a79e167f6
Samuel Osinubi <samobafemisayo@gmail.com>
2023-09-25 15:38:53 +0100

    bug check

--- middleware/error.js
+++ middleware/error.js
@@ -9,7 +9,7 @@ const errorHandler = (err, req, res, next) => {
 
    //Mongoose bad object
    if (err.name === "CastError") {
-      const message = `Resource not found`;
+      const message = `Resource was not found`;
       error = new ErrorResponse(message, 404);
    }
 
