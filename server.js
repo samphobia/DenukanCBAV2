9d88bcb5674e8b7a85627bc0a6a451d3ed6e371d
Samuel Osinubi <samobafemisayo@gmail.com>
2023-10-04 16:36:00 +0100

    edit

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use(cors())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false })
 
 
 
