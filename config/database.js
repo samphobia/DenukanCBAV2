9f79b2a88174ff29d2e16e2ac913dc62deba422f
Samuel Osinubi <samobafemisayo@gmail.com>
2023-10-05 10:01:29 +0100

    mysql bug on vercel fix

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
+const mysql2 = require('mysql2')
 
 const sequelize = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USER, process
   .env.MYSQL_ADDON_PASSWORD, {
   dialect: 'mysql',
+  dialectModule: mysql2,
   host: process.env.MYSQL_HOST
 });
 
