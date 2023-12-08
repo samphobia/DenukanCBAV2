8349cd0fd2ab9c1af541d177742cb6ef11fd9938
Samuel Osinubi <samobafemisayo@gmail.com>
2023-12-08 13:16:50 +0100

    get account details endpoint

--- routes/account.js
+++ routes/account.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const { createAccount, getAllAccounts, getAccountByType} = require('../controllers/account')
+const {
+  createAccount,
+  getAllAccounts,
+  getAccountByType,
+  getAccountDetails,
+} = require("../controllers/account");
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/createaccount", createAccount);
 
 router.post("/getallaccounts", getAllAccounts);
 
-router.get('/account/:accountType', getAccountByType);
+router.get("/account/:accountType", getAccountByType);
+
+router.get("/accountDetails/:accountNumber", getAccountDetails);
 
 module.exports = router;
